ce3477f9058adc4bc23f10e1f3d34bdf1b746a11
abraao.isvi <abraao.isvi@gmail.com>
2016-01-06 17:37:36 -0200

    dev

---
+++ bible-byz-port/book1/chapter1.js
@@ -0,0 +1,17 @@
+{
+  "number":1,
+  "language":"portuguese",
+  "origin":"greek",
+  "verses":[
+    {
+      "number":1,
+      "text":"texto#G1499 a /ser/ exibido#G1499 do verso /1/",
+      "ref":[
+        {"a":{"book":"Jo","chapter":1,"verse":1}}
+      ]
+    },
+    {
+      "number":2,
+      "text":"E a vida#G1499 /foi/ manifestada#G1499"
+    }]
+}
--- bible-byz-port/mod.js
+++ bible-byz-port/mod.js
@@ -1,36 +1,22 @@
-var Bible = {
+var Module = {
+  id: "bible-byz-port",
   type: "bible",
-
   books: {
-
-    "genesis": {//CODE
-      name: "Gênesis",//name that will be printed
-      order: 1,
+    "book1": {
+      name: "The Book 1",
       chapters: {
-        "1": {
-          number: 1,
-          verses: {
-            "1": {text:"texto 123 @dicionario#G5544 asdasdsa", ref: [{book:"X", chapter: "1", verse:"1"}] },
-            "2": {text:"texto 12345."}
-          }
-        }
+        "1": {file:"chapter1.js", verses:2},
+        "2": {file:"chapter2.js", verses:3}
       }
     },
-
-    "X": {
-      name: "X",
-      order: 2,
+    "book2": {
+      name: "The Book 2",
       chapters: {
-        "1": {
-          number: 1,
-          verses: {
-            "1": {text:"versiculo 1 palavra referenciada @dicionario G5544@ "},
-            "2": {text:"versiculo 2"}
-          }
-        }
+        "1": "chapter1.js",
+        "2": "chapter2.js",
       }
-    },
+    }
   }
 }
 
-module.exports = Bible;
+module.exports = Module;
